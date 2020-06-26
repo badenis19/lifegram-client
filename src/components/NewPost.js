8e929e8c8c0a8907160cdd2244eb4398861b4a6f
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-06-26 17:31:46 +0100

    form persits info bu with error in console - to solve

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { graphql } from 'react-apollo';
+import { flowRight as compose } from 'lodash'; 
 
-const NewPost = () => {
+/* Queries */
+import { getAllPostsQuery } from '../queries/queries';
 
-  return (
-    <div>
-      <h1>NewPost</h1>
-    </div>
-  )
-}
+/* Mutations */
+import { addPostMutation } from "../mutations/mutations"
 
-export default NewPost;
+const NewPost = (props) => {
+
+  let [description, setDescription] = useState("At the beach");
+  let [image, setImage] = useState("PPPPP");
+  let userId = "5ef50e5a2af31853d8f4964a";
+
+  const submitForm = (e) => {
+    console.log("running....");
+    e.preventDefault();
+    props.addPostMutation({ // addPostMutation refers to addPostMutation name (key) in the compose function at bottom of the page
+      variables: {
+        description: description,
+        img: image,
+        userId: userId
+      },
+      refetchQueries: [{ query: getAllPostsQuery }]
+    })
+  }
+
+    return (
+      <div className="share-form" onSubmit={(e) => submitForm(e)}>
+        <form action="">
+          <input type="text" placeholder="Write a caption.." name="post-description" /><br />
+          <button>Share</button>
+        </form>
+      </div>
+
+    )
+  }
+
+  export default compose(
+    graphql(addPostMutation, { name: "addPostMutation" }),
+    graphql(getAllPostsQuery, { name: "getAllPostsQuery" })
+  )(NewPost);
+
+  // export default graphql(addPostMutation)(NewPost);
