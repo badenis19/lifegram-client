08d8d98020fcba476c4a11d5e590921826540462
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-07-09 06:53:59 +0100

    added link to sign-in + signup page

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopNav = () => {
 
   return (
     <nav className="top-nav">
-      <h3>LIFEGRAM</h3>
+      <ul>
+        <Link to="/signup">
+          <li>Sign up</li>
+        </Link>
+
+        <Link to="/">
+          <li><strong>LIFEGRAM</strong></li>
+        </Link>
+
+        <Link to="/home">
+          <li>Sign in</li>
+        </Link>
+      </ul>
     </nav>
   )
 }
