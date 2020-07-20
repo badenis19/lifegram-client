268f8cad9087df5693ae9043215e9c17a33ababf
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-07-20 16:39:44 +0100

    search bar working, can look for all users

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -1,42 +1,66 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { graphql } from 'react-apollo';
 
-const SearchUser = () => {
+/* Queries */
+import { allUsersQuery } from "../queries/queries"
+import { empty } from 'apollo-boost';
+
+const SearchUser = (props) => {
 
   let history = useHistory();
 
   if (!Cookies.get('token')) {
     history.push('/signin');
+  };
+
+  let [userList, setUserList] = useState([]);
+  let [searchText, setSearchText] = useState("");
+  
+  let searchInput = document.getElementById("search-user-input");
+
+  const searchUsers = () => {
+
+    if(searchInput.value.length === 0){
+      console.log("empty");
+      setUserList([0]);
+    }
+    
+    const res = props.data.users.filter(user => {
+      return user.username.toLowerCase().includes(searchText);
+    })
+    setUserList(res);
   }
 
-  let [user, setUser] = useState("");
-  console.log(user);
-
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    // console.log("user");
+  const handleCancel = () => {
+    setSearchText('');
+    setUserList([]);
   }
-
-  // TO DO: Make query to fetch users, bind it to component, use data to populate the area
-  // const displayUserSearch = () => {
-  //   //
-  // }
 
   return (
     <div>
       <h1>Search User</h1>
-      {/* maybe create component for the search bar */}
-      <form onSubmit={(e) => handleSearchSubmit(e)}>
-        <input onChange={(e) => setUser(e.target.value)} type="text" placeholder="Search" />
-        <button>Search</button>
-      </form>
+      <input id="search-user-input" onKeyUp={() => searchUsers()} onChange={(e) => setSearchText(e.target.value.toLowerCase())} type="text" placeholder="Search" />
+      <button onClick={() => handleCancel()}>cancel</button>
 
-      <ul>
-        <li>List of user below:</li>
-      </ul>
+      <h3>List of user below:</h3>
+      {/* {<p>{userList.length}</p>} */}
+      {/* {!userList.length ? <p>No results found</p> : <p>{userList.length} result(s)</p>} */}
+      {/* {!userList.length ? <p>No results found</p> : <p>{userList.length} result(s)</p>} */}
+      {searchText.length > 0 &&
+        userList.map(user => {
+          return (
+            <div key={user._id}>
+              <p>username: {user.username}</p>
+              <img src={user.img} alt="" />
+            </div>
+          )
+        })
+      }
+
     </div>
   )
 };
 
-export default SearchUser;
+export default graphql(allUsersQuery)(SearchUser);
