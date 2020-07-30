40fd05790491b486bab24ccd85230c7636c04788
freddiewarren <fredswow@live.co.uk>
2020-07-30 09:58:58 +0100

    remmoved input labels

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -90,27 +90,23 @@ const Signup = () => {
       </div>
       <form className="signup-form" ref={formRef} action={url} method="POST" onSubmit={handleSubmit(onSubmit)} >
         <div>
-          {/* <label htmlFor="username">Username:</label> */}
           <input className="trial" id="username" type="text" placeholder="Username" name="username" ref={register({ required: true, maxLength: 15 })} />
           {errors.username && errors.username.type === 'required' && (< p > This is required</p>)}
           {errors.username && errors.username.type === 'maxLength' && (< p > This has a maximum length of 15</p>)}
         </div>
 
         <div>
-          {/* <label htmlFor="email">Email:&nbsp;</label> */}
           <input type="text" placeholder="Email" name="email" ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
           {errors.email && errors.email.type === 'required' && (< p > This is required</p>)}
           {errors.email && errors.email.type === 'pattern' && (< p > This is not a valid email address</p>)}
         </div>
 
         <div>
-          {/* <label htmlFor="password">Password:&nbsp;</label> */}
           <input type="password" placeholder="Password" name="password" ref={register({ required: true })} />
           {errors.password && (< p > This is required</p>)}
         </div>
 
         <div>
-          {/* <label htmlFor="age">Age:&nbsp;</label> */}
           <input type="number" placeholder="Age" name="age" ref={register({ required: true, max: 999, min: 1 })} />
           {errors.age && errors.age.type === 'required' && (< p > This is required</p>)}
           {errors.age && errors.age.type === 'max' && (< p > surely your not 1000years old or over!</p>)}
