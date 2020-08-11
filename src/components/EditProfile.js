a5a6745a1edfb974796e449341f9b693fecceefa
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-08-11 07:14:24 +0100

    save progress, trying to use reset to prepopulate form

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,47 +1,117 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { graphql } from 'react-apollo';
-import UserForm from './UserForm';
+// import UserForm from './UserForm';
 import { useHistory } from 'react-router-dom';
 import { getMyProfileQuery } from '../queries/queries';
 import Cookies from 'js-cookie';
 import { SignedInContext } from "../App";
+import { useForm } from "react-hook-form";
 // Connected user data
 
 const EditProfile = (props) => {
 
   let history = useHistory();
 
+  // let [preloadedValues, setPreloadedValues] = useState("");
+
+  // const { register, handleSubmit, errors } = useForm({
+  //   defaultValues: preloadedValues
+  // });
+
+   const { register, handleSubmit, errors, reset } = useForm();
+
   let { updateSignIn } = useContext(SignedInContext);
-  
+
   if (!Cookies.get('token')) {
     history.push('/userprofile');
   } else {
     updateSignIn(true);
   }
 
-  let data = props.data;
-  // console.log(data)
-
-  const displayForm = () => {
-    if (data.loading) {
-      return (<p>Loading...</p>)
-    } else if (!data.myProfile) {
-      return (<p>No users have been loaded. Contact admin.</p>)
-    } else if (!data.loading) {
-      return (
-        <div>
-          <UserForm preloadedValues={data.myProfile} />
-        </div>
-      )
-    }
+  const onSubmit = () => {
+    console.log("ok");
   }
+
+  let data = props.data;
+  console.log(data)
+
+  useEffect( () => {
+    const fetchUserData = async () => {
+      if (data.loading) {
+        console.log("loading")
+      } else if (!data.myProfile) {
+        console.log("No user data")
+      } else if (!data.loading) {
+        console.log("the data:", data)
+        const result = await data.myProfile
+        // return await data.myProfile
+        console.log("::", result)
+        return result
+      }
+    }
+
+    console.log(fetchUserData())
+
+    // reset(result);
+  },[data])
+
+  // useEffect(async () => {
+  //   const result = await fetch('./api/formValues.json'); // result: { firstName: 'test', lastName: 'test2' }
+  //   reset(result); // asynchronously reset your form values
+  // }, [reset])
+
+  // useEffect(async () => {
+  //   const result = await fetch('./api/formValues.json'); // result: { firstName: 'test', lastName: 'test2' }
+  //   reset(result); // asynchronously reset your form values
+  // }, [reset])
 
   return (
     <div>
       <div className="sign-in-up-intro">
         <h3>Edit your profile</h3>
       </div>
-      {displayForm()}
+      <form className="edit-user-form" onSubmit={handleSubmit(onSubmit)} >
+        {errors.serverError && errors.serverError.message}
+
+        <div className="change-img">
+        {/* { preloadedValues &&  <img src= {preloadedValues.img} alt=""/>} */}
+          <p >Change profile photo</p>
+        </div>
+
+        <div>
+          <input className="trial" id="username" type="text" placeholder="Username" name="username" ref={register({ required: true, maxLength: 15 })} />
+          {errors.username && errors.username.type === 'required' && (< p > This is required</p>)}
+          {errors.username && errors.username.type === 'maxLength' && (< p > This has a maximum length of 15</p>)}
+        </div>
+
+        <div>
+          <input type="text" placeholder="Email" name="email" ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
+          {errors.email && errors.email.type === 'required' && (< p > This is required</p>)}
+          {errors.email && errors.email.type === 'pattern' && (< p > This is not a valid email address</p>)}
+        </div>
+
+        <div>
+          <input type="number" placeholder="Age" name="age" ref={register({ required: true, max: 999, min: 1 })} />
+          {errors.age && errors.age.type === 'required' && (< p > This is required</p>)}
+          {errors.age && errors.age.type === 'max' && (< p > surely your not 1000years old or over!</p>)}
+        </div>
+
+        <div>
+          <input className="trial" id="description" type="text" placeholder="Description" name="description" ref={register({ maxLength: 50 })} />
+          {errors.description && errors.description.type === 'required' && (< p > This is required</p>)}
+          {errors.description && errors.description.type === 'maxLength' && (< p > This has a maximum length of 15</p>)}
+        </div>
+
+        <div>
+          <input type="password" placeholder="Password" name="password" ref={register({ required: true })} />
+          {errors.password && (< p > This is required</p>)}
+        </div>
+
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+      {/* {displayForm()} */}
     </div>
   )
 };
