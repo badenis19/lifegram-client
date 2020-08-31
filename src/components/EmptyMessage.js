a2873e8c36b632b92d723f62b4a2f66d2b1f0129
freddiewarren <fredswow@live.co.uk>
2020-08-31 19:57:08 +0100

    profile default no post, styled.

--- src/components/EmptyMessage.js
+++ src/components/EmptyMessage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Icon, InlineIcon } from '@iconify/react';
+import armFlex from '@iconify/icons-mdi/arm-flex';
+
 
 const EmptyMessage = ({ message, entity }) => {
 
     if (entity === "user-profile") {
         return (
-            <div className="">
-                <p>{message} <Link to={`/newpost`}>Upload first post</Link></p>
+            <div className="message-container">
+                <Icon icon={armFlex} />
+                <p>{message}</p>
+                <p><Link to={`/newpost`}>Upload first post</Link></p>
             </div>
         )
     } else if (entity === "post-feed") {
