5928b6171e06386f633dad22b7ea72193b808b3f
freddiewarren <fredswow@live.co.uk>
2020-08-31 13:52:49 +0100

    created empty message component w/ bruno

---
+++ src/components/EmptyMessage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const EmptyMessage = ({ message, entity }) => {
+
+    if (entity === "user-profile") {
+        return (
+            <div className="">
+                <p>{message} <Link to={`/newpost`}>Add new post.</Link></p>
+            </div>
+        )
+    } else if (entity === "post-feed") {
+        return (
+            <div className="">
+                <p>{message}</p>
+            </div>
+        )
+    } else if (entity === "search") {
+        return (
+            <div className="">
+                <p>{message}</p>
+            </div>
+        )
+    }
+}
+
+EmptyMessage.propTypes = {
+    message: PropTypes.string.isRequired,
+    entity: PropTypes.string.isRequired
+}
+
+export default EmptyMessage;
